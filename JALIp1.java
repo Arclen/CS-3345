313ed50fcc116a9f649f317f1d28c7631c93b1ff
Jacob Liou <jacob.c.liou@gmail.com>
2017-08-31 10:28:34 -0500

    Add files via upload

---
+++ JALIp1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+/*
+ CS 3345
+ Project 1
+ Used compiler Java 1.8.0_131 
+ @author Jacob Liou
+ date 8/31/2017
+*/
+
+class JALIp1 {
+    
+	public static void main(String[] args) throws IOException {
+	    //Scanner in = new Scanner(new File("data1.txt"));
+	    Scanner in = new Scanner(System.in);
+	}
+}
