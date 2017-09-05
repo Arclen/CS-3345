b74ebb2d2367534e46f6213f4e2bc5dd200cc018
Jacob Liou <jacob.c.liou@gmail.com>
2017-09-05 00:13:19 -0500

    CS isn't so bad, this is kinda fun actually

--- HW1.java
+++ HW1.java
@@ -1,10 +1,31 @@
+import java.util.*;
+import java.io.*;
+  /**
+  * CS 3345 
+  * Project 1
+  * Used compiler Java 1.8.0_131
+  * @author Jacob Liou
+  * 9/1/17
+  */
+
 public class HW1 {
     public static void main(String[] args) {
       System.out.println("Node test: ");
-      System.out.println("Linked list test: ");
+      Node test_tail;
+      test_tail = null;
+      Node test_head = new Node(test_tail, 0);
+      Node test_val = new Node(test_tail, 1);  
+      test_head.putNext(test_val);
+      List list = new List();
+      //list.displayList();
+      list.insertAtTail(3);
+      list.insertAtTail(4);
+      list.insertAtTail(5);
+      list.insertAtTail(6);
+      list.displayList();
     }
 
-    class Node {
+    static class Node {
       private Node next;
       private int key;
 
@@ -23,23 +44,32 @@ public class HW1 {
       }
     }
 
-    class List {      // assume the class does not use a dummy Node
+    static class List {      // assume the class does not use a dummy Node
     private Node head;
     List() {           // constructor
+    }
+    public void displayList() {
+      Node n = head;
+      System.out.print("List contents: ");
+      while(n != null) {
+        System.out.print(n.getKey() + " ");
+        n = n.getNext();
+      }
     }
     boolean exists(int ky) {     // returns true if ky is in the list 
       return true;
     }
     void insertAtHead(int ky) {  // inserts at the beginning of the list
-    
+
     }
-      void insertAtTail(int ky) { // inserts at the end of the list
+    void insertAtTail(int ky) { // inserts at the end of the list
       if(head == null)
         head = new Node(head, ky);
       else {
         Node temp = head;
         while(temp.getNext() != null) {
           temp = temp.getNext();
+          System.out.print("debug");
         }
         temp.putNext(new Node(temp, ky));
       }
@@ -52,10 +82,12 @@ public class HW1 {
         if(head.getKey() == ky)
           head = head.getNext();  // remove first element
         else {
-          Node prev = new Node(head, null);
-          while(prev.getNext() != null && prev.getNext().getKey() != ky) {
-            
+          Node ref = new Node(head, 0);
+          while(ref != null && ref.getKey() != ky) {
+            ref = ref.getNext();
           }
+          if(ref != null)
+            ref.putNext(ref.getNext());
         }
       }
     }
@@ -66,23 +98,51 @@ public class HW1 {
       return 1;
     }
     int maxElement() {
-      return 1;
+      return maxElement(head);
     }     
     int sum() {
-      return 1;
+      return sum(head);
     }            
     int length() {
-      return 1;
+      return length(head);
+    }
+    public void recursiveDelete(int ky) {
+      recursiveDelete(ky, head);
     }         
 
     private int maxElement(Node x) {
-      return 1;
+      if(x != null) { // return -1 if list is empty
+        if(x.getNext() == null) // end of list
+          return x.getKey();
+        else {
+          int max = maxElement(x.getNext());
+          if(max < x.getKey())
+            return x.getKey();
+          return max;
+        }
+      }
+      return -1;
     }
     private int sum(Node x) {
-      return 1;
+      if(x != null) // return -1 if list is empty
+      {
+        if(x.getNext() == null) // end of list
+          return x.getKey();
+        return x.getKey() + sum(x.getNext());
+      }
+      return -1;
     }
     private int length(Node x) {
-      return 1;
+      if(x != null) { // return 0 if list is empty
+        if(x.getNext() == null)
+          return 1;
+        else return 1 + length(x.getNext());
+      }
+      return 0;
     }
+    private Node recursiveDelete(int ky, Node n) {
+      return n;
+    }
+
   }
 }
