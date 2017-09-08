b842c826300e12cddba076e5d10bed724f10d1f7
Jacob Liou <jacob.c.liou@gmail.com>
2017-09-07 20:18:45 -0500

    Jen says it isn't that hard, so I better finish lol

--- Project 1/JALIp1.java
+++ Project 1/JALIp1.java
@@ -3,21 +3,22 @@ import java.io.*;
 /*
  CS 3345
  Project 1
- Used compiler Java 1.8.0_131 
+ Used compiler Java 1.8.0_131
  @author Jacob Liou
  date 8/31/2017
 */
 
 class JALIp1 {
-    
+
 	public static void main(String[] args) throws IOException {
-	    //Scanner in = new Scanner(new File("data1.txt"));
-	    Scanner in = new Scanner(System.in);
-	    Totem marioKart = new Totem();
-		marioKart.insertAtTail(21);
+	  //Scanner in = new Scanner(new File("data1.txt"));
+	  Scanner in = new Scanner(System.in);
+	  Totem marioKart = new Totem();
+    marioKart.insertAtTail(21);
+    System.out.println("Welcome to your reckoning baybeeeeeee");
 	}
     static class Node {
-	private int ID; 
+	private int ID;
 	private Node next;
 
 	Node(int ID, Node nxt) {
@@ -44,7 +45,7 @@ class JALIp1 {
 
     static class Totem {
 	private Node head, tail;
-	
+
 	void insertAtTail(int ID) {
 	    Node ins = new Node(ID, head);
 	    while(ins.getNext() != tail) {
@@ -68,7 +69,9 @@ class JALIp1 {
 	}
 
 	void insert(int ID, int posn) {
-	    
+	    if(head != null) {
+
+	    }
 	}
     }
 }
