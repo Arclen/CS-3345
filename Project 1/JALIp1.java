0b0e74bf0988113edd9565d6cac4ddd07936e2ed
Jacob Liou <jacob.c.liou@gmail.com>
2017-09-15 22:02:08 -0500

    I did it! But I should still finish earlier next time

--- Project 1/JALIp1.java
+++ Project 1/JALIp1.java
@@ -3,7 +3,7 @@ import java.io.*;
 /*
  CS 3345
  Project 1
- javac version "1.8.0_141-2-redhat"
+ javac 1.8.0_141-2-redhat
  java version "1.8.0_131"
  @author Jacob Liou
  date 8/31/2017
@@ -12,38 +12,39 @@ import java.io.*;
 class JALIp1 {
 
 	public static void main(String[] args) throws IOException{
-	  Scanner in = new Scanner(new File("carsd1.txt"));
-	  // Scanner in = new Scanner(System.in);
-	  Totem tot = new Totem();
+	  Scanner in = new Scanner(new File("carsd1.txt"));	// for testing
+	  // Scanner in = new Scanner(System.in);									// for submission
 
-		boolean done = false;
-		boolean debug = false;
+	  Totem tot = new Totem();
+		boolean debug = false;		// Whether or not to output the car positions after every input
+		boolean done = false;			// I made a variable called done and set it to false
+															// as opposed to making a variable called notDone
+															// and setting it to true because I am a rebel
+
 		while(!done) {
 			String line = in.nextLine();
 			String [] tokens = line.split(" ");
 			switch(tokens[0]) {
-				case "START":
+				case "START":	// Initialize the linked list
 					for(int i=3; i <= Integer.parseInt(tokens[1])+4; i++) {
-						// System.out.print(tokens[i-1] + " ");
 						tot.insert(Integer.parseInt(tokens[i-2]), i-3);
 					}
-					// System.out.println("\nRace order: ");
 					if(Integer.parseInt(tokens[2]) == 1)
 						debug = true;
 				break;
-				case "DROPOUT":
+				case "DROPOUT":	// Remove car based on ID
 					tot.remove(Integer.parseInt(tokens[1]));
 				break;
-				case "OVERTAKE":
+				case "OVERTAKE":	// A car with certain ID passes the one in front of it
 					tot.swap(Integer.parseInt(tokens[1]));
 				break;
-				case "PITSTOP":
+				case "PITSTOP":	// Car vanishes from the race like the Heart of Gold starship
 					tot.remove(Integer.parseInt(tokens[1]));
 				break;
-				case "PITRETURN":
+				case "PITRETURN":	// Car which left earlier appears out of nowhere like the Heart of Gold starship
 					tot.insert(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2])+2);
 				break;
-				case "CRASH":
+				case "CRASH":	// Car crashes like the Heart of Gold starship
 					for(int i=1; i<tokens.length; i++) {
 							 tot.remove(Integer.parseInt(tokens[i]));
 					}
@@ -52,7 +53,7 @@ class JALIp1 {
 					done = true;
 				break;
 			}
-			if(debug && tokens[0] != "END") {
+			if(debug) {
 				tot.contents();
 			}
 		}
@@ -96,10 +97,10 @@ class JALIp1 {
 
 		void insertAtTail(int ID) {
 				size++;
-				if(head != null) {
+				if(head != null) { // Make sure the totem isn't null
 					Node ref = head;
 					if(ref.getNext() == tail) {
-						ref.putNext(new Node(ID, tail));
+						ref.putNext(new Node(ID, tail)); // If the totem is just head and tail, put the node in between them
 					}
 				  else {
 						boolean atTail = false;
@@ -119,14 +120,14 @@ class JALIp1 {
 		void insert(int ID, int posn) {
 			Node ins = new Node(ID, null);
 			if(head.getNext() == tail) // The totem is empty
-			head = ins;
+				head = ins;
 			Node ref = head;
 			posn = posn-1;
 			for(int i=1; i<size; i++) {
 				if(i == posn) {
-					Node temp = ref.getNext();
-					ref.putNext(ins);
-					ins.putNext(temp);
+					Node temp = ref.getNext();	// Hold onto the new Node's future rear neighbor
+					ref.putNext(ins);						// Place the new Node behind the reference
+					ins.putNext(temp);					// Put temp behind the new Node
 				}
 				ref = ref.getNext();
 			}
@@ -135,14 +136,14 @@ class JALIp1 {
 
 		void remove(int ID) {
 	    if(head != null) {
-				Node looker = head;
+				Node ref = head;
 				Node prev = null;
-				while(looker != null && looker.getID() != ID) {
-				    prev = looker;
-				    looker = looker.getNext();
+				while(ref != null && ref.getID() != ID) {	// Iterate through the list until ID is found
+				    prev = ref;
+				    ref = ref.getNext();
 				}
-				if(looker != null)
-				    prev.putNext(looker.getNext());
+				if(ref != null)
+				    prev.putNext(ref.getNext());	// Now prev points to what ref points to, cutting out ref
 	    }
 			size--;
 		}
@@ -151,9 +152,9 @@ class JALIp1 {
 			Node ref = head;
 			if(head != null) {
 				while(ref.getNext().getNext() != null) {
-					if(ref.getNext().getNext().getID() == ID) {
-						Node temp1 = ref.getNext();
-						Node temp2 = ref.getNext().getNext();
+					if(ref.getNext().getNext().getID() == ID) {	// There are 4 nodes involved in swap
+						Node temp1 = ref.getNext();								// 3 refernces must be changed to preserve
+						Node temp2 = ref.getNext().getNext();			// order and prevent an infinite loop
 						Node temp3 = ref.getNext().getNext().getNext();
 						ref.getNext().getNext().putNext(temp1);
 						ref.getNext().putNext(temp3);
@@ -172,7 +173,7 @@ class JALIp1 {
 			return head;
 		}
 
-		void contents() {
+		void contents() { // Print out the contents of the list, for debug and the end of the race
 			Node ref = head;
 			while(ref.getNext() != null) {
 				System.out.print(ref.getNext().getID()+ " ");
