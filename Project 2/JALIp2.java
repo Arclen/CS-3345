a699b1b26a40999167084c641638c63a3d9ddf98
Jacob Liou <jacob.c.liou@gmail.com>
2017-09-26 12:09:00 -0500

    Laying some ground work

---
+++ Project 2/JALIp2.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+/*
+ CS 3345
+ Project 1
+ javac 1.8.0_141-2-redhat
+ java version "1.8.0_131"
+ @author Jacob Liou
+ date 8/31/2017
+*/
+class JALIp2 {
+
+	public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+
+  public class TreeNode {
+    int key;
+    TreeNode leftChild;
+    TreeNode rightChild;
+
+
+  }
+
+  public class BST {
+    BST() {
+
+    }
+
+    void makeEmpty() {
+
+    }
+
+    boolean insert(int key) {
+      return true;
+    }
+
+    boolean delete(int key) {
+      return false;
+    }
+  }
+}
