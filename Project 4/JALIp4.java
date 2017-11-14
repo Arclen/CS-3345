19e96e075c4141d7394ebe677d2bdbebb99a149e
Jacob Liou <jacob.c.liou@gmail.com>
2017-11-14 14:38:19 -0600

    This is my last chance to finish early, I'm gonna do it

---
+++ Project 4/JALIp4.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+/*
+ CS 3345
+ Project 4
+ javac 1.8.0_141-2-redhat
+ java version "1.8.0_131"
+ date 12/6/2017
+*/
+class JALIp4 {
+
+  /*
+  DO NOT FORGET TO SWITCH TO SUBMISSION MODE
+  DO NOT FORGET TO SWITCH TO SUBMISSION MODE
+  DO NOT FORGET TO SWITCH TO SUBMISSION MODE
+  DO NOT FORGET TO SWITCH TO SUBMISSION MODE
+  DO NOT FORGET TO SWITCH TO SUBMISSION MODE
+  DO NOT FORGET TO SWITCH TO SUBMISSION MODE
+  DO NOT FORGET TO SWITCH TO SUBMISSION MODE
+  DO NOT FORGET TO SWITCH TO SUBMISSION MODE
+  DO NOT FORGET TO SWITCH TO SUBMISSION MODE
+  DO NOT FORGET TO SWITCH TO SUBMISSION MODE
+  DO NOT FORGET TO SWITCH TO SUBMISSION MODE
+  */
+
+  public static void main(String[] args) throws IOException{
+    // Scanner in = new Scanner(new File("d1.txt"));	// for testing
+	  Scanner in = new Scanner(System.in);				// for submission
+
+  }
+
+}
